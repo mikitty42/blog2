71d3788d8dd8281556bc25352040538b14d098d7
takezawamiki <mikiaaamiki@yahoo.co.jp>
2024-06-10 14:47:59 +0900

    create routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'blogs/index'
-  get 'blogs/new'
-  get 'blogs/edit'
-  get 'blogs/confirm'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :blogs
 end
